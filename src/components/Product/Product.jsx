eba93ad630ffaa3d33f6c3fd42442a5d8a07528d
Aditta Chakraborty <adittacse@gmail.com>
2023-03-30 18:29:40 +0600

    product design and add to cart button added

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -6,10 +6,15 @@ const Product = (props) => {
     return (
         <div className="product">
             <img src={img} alt="shoe image"/>
-            <h3>{name}</h3>
-            <h4>Price: ${price}</h4>
-            <p>Manufacture: {seller}</p>
-            <p>Ratings: {ratings} star</p>
+            <div className="product-information">
+                <h6 className="product-title">{name}</h6>
+                <h4>Price: ${price}</h4>
+                <p className="manufacture">Manufacture: {seller}</p>
+                <p className="rating">Rating: {ratings} star</p>
+            </div>
+            <div>
+                <button className="btn-add-to-cart">Add to Cart</button>
+            </div>
         </div>
     );
 };
