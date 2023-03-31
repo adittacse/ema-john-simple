a58983dff836d48d2996b1c6ddbbae10f07aca4c
Aditta Chakraborty <adittacse@gmail.com>
2023-03-31 06:02:35 +0600

    show data from cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,10 +7,16 @@ const Cart = (props) => {
 
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
 
     for (const product of cart) {
-        totalPrice += product.price;
+        // product.quantity = product.quantity || 1;
+        if (product.quantity === 0) {
+            product.quantity = 1;
+        }
+        totalPrice += product.price * product.quantity;
         totalShipping += product.shipping;
+        quantity += product.quantity;
     }
 
     const tax = totalPrice * 7 / 100;
@@ -20,7 +26,7 @@ const Cart = (props) => {
         <div className="cart">
             <h4 className="cart-title">Order Summery</h4>
             <div className="cart-information">
-                <p>Selected Items: {cart.length}</p>
+                <p>Selected Items: {quantity}</p>
                 <p>Total Price: ${totalPrice}</p>
                 <p>Total Shipping Charge: ${totalShipping}</p>
                 <p>Tax: ${tax.toFixed(2)}</p>
