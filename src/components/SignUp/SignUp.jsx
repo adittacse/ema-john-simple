f8c5febf370a0d6c67345ab1835a5c0ac1f69e5b
Aditta Chakraborty <adittacse@gmail.com>
2023-04-25 11:30:39 +0600

    signup page design updated

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import "./SignUp.css";
+import {Link} from "react-router-dom";
 
 const SignUp = () => {
     return (
-        <div>
-            <h2>Please Sign Up!</h2>
+        <div className="form-container">
+            <h4 className="form-title">Sign Up</h4>
+            <form>
+                <div className="form-control">
+                    <label htmlFor="">Email</label>
+                    <input type="email" name="email" id="email" required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="">Password</label>
+                    <input type="password" name="password" id="password" required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="">Confirm Password</label>
+                    <input type="password" name="confirm-password" id="confirm-password" required />
+                </div>
+                <input className="btn btn-submit" type="submit" value="Sign Up"/>
+            </form>
+            <p><small>Already have an account? <span><Link to="/login">Login</Link></span></small></p>
         </div>
     );
 };
