138f843bd3fbe95a695356ed18d01f60809b538e
Aditta Chakraborty <adittacse@gmail.com>
2023-04-25 11:02:54 +0600

    login page design updated

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,8 +3,19 @@ import "./Login.css";
 
 const Login = () => {
     return (
-        <div>
-            <h2>Login Page</h2>
+        <div className="form-container">
+            <h4 className="form-title">Login</h4>
+            <form>
+                <div className="form-control">
+                    <label htmlFor="">Email</label>
+                    <input type="email" name="email" id="email" required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="">Password</label>
+                    <input type="password" name="password" id="password" required />
+                </div>
+                <input className="btn btn-submit" type="submit" value="Login"/>
+            </form>
         </div>
     );
 };
