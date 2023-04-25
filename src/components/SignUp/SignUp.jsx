f57c04715aa96388b7af93975cfd1acdbd45bef4
Aditta Chakraborty <adittacse@gmail.com>
2023-04-25 13:20:04 +0600

    signup page data collected and design updated

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,27 +1,49 @@
-import React from 'react';
-import "./SignUp.css";
+import React, {useState} from 'react';
 import {Link} from "react-router-dom";
+import "./SignUp.css";
 
 const SignUp = () => {
+    const [error, setError] = useState("");
+    
+    const handleSignUp = (event) => {
+        event.preventDefault();
+        setError("");
+        
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        const confirmPassword = form.confirmPassword.value;
+        console.log(email, password, confirmPassword);
+        
+        if (password !== confirmPassword) {
+            setError("Your Password Didn't Match!")
+            return;
+        } else if (password.length < 6) {
+            setError("Your Password Must Be 6 Characters Long!");
+            return;
+        }
+    }
+    
     return (
         <div className="form-container">
             <h4 className="form-title">Sign Up</h4>
-            <form>
+            <form onSubmit={handleSignUp}>
                 <div className="form-control">
-                    <label htmlFor="">Email</label>
+                    <label htmlFor="email">Email</label>
                     <input type="email" name="email" id="email" required />
                 </div>
                 <div className="form-control">
-                    <label htmlFor="">Password</label>
+                    <label htmlFor="password">Password</label>
                     <input type="password" name="password" id="password" required />
                 </div>
                 <div className="form-control">
-                    <label htmlFor="">Confirm Password</label>
-                    <input type="password" name="confirm-password" id="confirm-password" required />
+                    <label htmlFor="confirmPassword">Confirm Password</label>
+                    <input type="password" name="confirmPassword" id="confirmPassword" required />
                 </div>
                 <input className="btn btn-submit" type="submit" value="Sign Up"/>
             </form>
-            <p><small>Already have an account? <span><Link to="/login">Login</Link></span></small></p>
+            <p className="login-text"><small>Already have an account? <span><Link to="/login">Login</Link></span></small></p>
+            <p className="text-error">{error}</p>
         </div>
     );
 };
